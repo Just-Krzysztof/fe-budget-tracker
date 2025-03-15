bc75327b41bbef84f89b62ed9b0630092fe7726f
Krzysztof <krzysztofjustgdynia@gmail.com>
2025-03-15 12:28:10 +0100

    fix a liitle bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import PrivateRoute from './components/auth/PrivateRoute'
@@ -9,30 +9,30 @@ import './App.css'
 import ExpensesPage from './pages/ExpensesPage'
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false)
+  // const [isDarkMode, setIsDarkMode] = useState(false)
 
-  // Sprawdzamy preferencje trybu ciemnego przy ładowaniu komponentu
-  useEffect(() => {
-    const isCurrentlyDark = document.documentElement.classList.contains('dark')
-    setIsDarkMode(isCurrentlyDark)
-  }, [])
+  // // Sprawdzamy preferencje trybu ciemnego przy ładowaniu komponentu
+  // useEffect(() => {
+  //   const isCurrentlyDark = document.documentElement.classList.contains('dark')
+  //   setIsDarkMode(isCurrentlyDark)
+  // }, [])
 
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode)
-    document.documentElement.classList.toggle('dark')
-    // Zapisujemy preferencje użytkownika
-    if (document.documentElement.classList.contains('dark')) {
-      localStorage.setItem('theme', 'dark')
-    } else {
-      localStorage.setItem('theme', 'light')
-    }
-  }
+  // const toggleDarkMode = () => {
+  //   setIsDarkMode(!isDarkMode)
+  //   document.documentElement.classList.toggle('dark')
+  //   // Zapisujemy preferencje użytkownika
+  //   if (document.documentElement.classList.contains('dark')) {
+  //     localStorage.setItem('theme', 'dark')
+  //   } else {
+  //     localStorage.setItem('theme', 'light')
+  //   }
+  // }
 
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+          {/* <button onClick={toggleDarkMode}>Toggle Dark Mode</button> */}
           {/* Publiczne ścieżki */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
